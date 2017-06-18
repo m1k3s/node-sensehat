c340abba2a6f0e165551fa054d875bee491718c0
Michael Sheppard <pbrane0@gmail.com>
2017-06-18 09:48:23 -0400

    major overhaul to query system

--- server/routes/queries.js
+++ server/routes/queries.js
@@ -1,25 +1,70 @@
-var express = require('express');
-var router = express.Router();
-var PythonShell = require('python-shell');
-const pg = require('pg');
-const path = require('path');
-const connectionString = 'postgres://localhost:5432/serverstats';
-PythonShell.defaultOptions = { scriptPath: 'server/python/' };
+function getSensorData(req, res, next) {
+    const results = [];
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if (err) {
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+        // SQL Query > Select Data
+        const query = client.query('SELECT * FROM environmental ORDER BY id ASC;');
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+}
 
-var db = require('queries');
+// post to call the python script using nodejs python-shell
+function postRainbow(req, res, next) {
+    PythonShell.run('rainbow.py', function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        //console.log('results: %j', results);
+    });
+}
 
-// GET home page
-router.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '..', '..', 'client', 'views', 'index.html'));
-});
+function postSparkle(req, res, next) {
+    PythonShell.run('sparkle.py', function(err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        //console.log('results: %j', results);
+    });
+}
 
-router.get('/db/env_sensors', db.getSensorData);
-router.post('/rainbow', db.postRainbow);
-router.post('/sparkle', db.postSparkle);
-router.get('/db/loadavg', db.getLoadAvg);
+function getLoadAvg(req, res, next) {
+    const results = [];
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if (err) {
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+        // SQL Query > Select Data
+        const query = client.query('SELECT * FROM loadavg ORDER BY id ASC;');
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+}
 
 // get only the last 'rows' number of rows from db
-router.get('/db/env_sensors/:rows', function(req, res, next) {
+function getSensorDataRows(req, res, next) {
     const results = [];
     const rows = req.params.rows;
     // Get a Postgres client from the connection pool
@@ -42,9 +87,9 @@ router.get('/db/env_sensors/:rows', function(req, res, next) {
             return res.json(results);
         });
     });
-});
+}
 
-router.get('/db/env_sensors/:start:end', function(req, res, next) {
+function getSensorDataRange(req, res, next) {
     const results = [];
     const start = req.params.start;
     const end = req.params.end;
@@ -68,9 +113,9 @@ router.get('/db/env_sensors/:start:end', function(req, res, next) {
             return res.json(results);
         });
     });
-});
+}
 
-router.get('/db/loadavg/:rows', function(req, res, next) {
+function getLoadAvgRows(req, res, next) {
     const results = [];
     const rows = req.params.rows;
     // Get a Postgres client from the connection pool
@@ -93,9 +138,9 @@ router.get('/db/loadavg/:rows', function(req, res, next) {
             return res.json(results);
         });
     });
-});
+}
 
-router.get('/serverinfo', function (req, res, next) {
+function getServerInfo(req, res, next) {
     let results = '';
     pyshell = new PythonShell('getserverinfo.py', {mode: 'json'});
 
@@ -108,9 +153,9 @@ router.get('/serverinfo', function (req, res, next) {
         //console.log('finished getting server info');
         return res.json(results);
     });
-});
+}
 
-router.get('/sysinfo', function (req, res, next) {
+function getSystemInfo(req, res, next) {
     let results = '';
     pyshell = new PythonShell('getsysteminfo.py', {mode: 'json'});
 
@@ -123,9 +168,9 @@ router.get('/sysinfo', function (req, res, next) {
         //console.log('finished getting server info');
         return res.json(results);
     });
-});
+}
 
-router.get('/db/netstats', function(req, res, next) {
+function getNetStats(req, res, next) {
     const results = [];
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
@@ -147,9 +192,9 @@ router.get('/db/netstats', function(req, res, next) {
             return res.json(results);
         });
     });
-});
+}
 
-router.get('/db/netstats/:rows', function(req, res, next) {
+function getNetStatsRows(req, res, next) {
     const results = [];
     const rows = req.params.rows;
     // Get a Postgres client from the connection pool
@@ -172,9 +217,9 @@ router.get('/db/netstats/:rows', function(req, res, next) {
             return res.json(results);
         });
     });
-});
+}
 
-router.get('/db/diskstats', function(req, res, next) {
+function getDiskStats(req, res, next) {
     const results = [];
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
@@ -196,9 +241,9 @@ router.get('/db/diskstats', function(req, res, next) {
             return res.json(results);
         });
     });
-});
+}
 
-router.get('/db/diskstats/:rows', function(req, res, next) {
+function getDiskStatsRows(req, res, next) {
     const results = [];
     const rows = req.params.rows;
     // Get a Postgres client from the connection pool
@@ -221,8 +266,54 @@ router.get('/db/diskstats/:rows', function(req, res, next) {
             return res.json(results);
         });
     });
-});
+}
 
+function getSensorDataToday(req, res, next) {
+    const results = [];
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if (err) {
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+        // SQL Query > Select only todays rows
+        const query = client.query('SELECT * FROM environmental WHERE timestamp > TIMESTAMP \'today\'');
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+}
 
-module.exports = router;
+function getSensorDataYesterday(req, res, next) {
+    const results = [];
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if (err) {
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+        // SQL Query > Select only todays rows
+        const query = client.query('SELECT * FROM environmental WHERE timestamp > TIMESTAMP \'yesterday\' AND timestamp < TIMESTAMP \'today\'');
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+}
+
 
